38c86cc953d2cf2d7191f7db9b783c76aa79206d
Mehmood Hassan <mehmood.mh56@gmail.com>
2026-06-25 13:52:49 +0000

    Understanding jsx along with injection of js, how React create element

--- Vite_React/my-react-app/src/App.jsx
+++ Vite_React/my-react-app/src/App.jsx
@@ -2,9 +2,20 @@ import './App.css'
 
 function App() {
 
+  // Can we Write js in jsx return() yes but only final answer not set of statement(s)
+
+  const name = "King Babar Azam"
+
   return (
+
+     // we cann't write set of statements in this line we just write answer in this line
+     // if we write any comment inside this <> .. </> then it displays on the screen
+     
     <>
-    <h1>Hello from App</h1>
+  
+    <h1>Hello {name}!</h1>
+    
+    
     </>
   )
 }
--- Vite_React/my-react-app/src/main.jsx
+++ Vite_React/my-react-app/src/main.jsx
@@ -1,10 +1,70 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+// for testing
+
+function My(){ // never use lowercase letters
+  return(
+    <div>
+      <h3>
+        Hello from main.jsx
+      </h3>
+    </div>
+  )
+}
+
+
+
+// const React_Element = {
+//     type: "a",
+//     props: {
+//         href: "https://www.robotstxt.org/robotstxt.html",
+//         target: "_blank",
+//     },
+//     children: "Click Me",
+// }
+
+
+
+// if You want to check where {injected variable} write in this 
+
+const name = "Babar"
+
+
+const React_Element2 = React.createElement(
+  'a',
+  {href: "https://www.robotstxt.org/robotstxt.html", target: "_blank"},
+  "Click ",
+  name,
+
+)
+
+
+const Element = (
+  <a href='https://www.robotstxt.org/robotstxt.html' target='_blank'>
+    Click Me
+  </a>
+)
+
+
+createRoot(document.getElementById('root'))
+.render(
+
+  // <My />,  // if don't add comma it raise error
+
+  // My(),   // Not Recommended
+
+  // <React_Element />,  // it need function not object
+  
+  // Element,
+
+
+  // React_Element2,
+
+
   <StrictMode>
-    <App />
+  <App />
   </StrictMode>,
 )
